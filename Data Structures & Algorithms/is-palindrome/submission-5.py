150771f2316cd1f769974c29c3e370827f0d6763
Børge Bjørnstadjordet <57920815+borgebj@users.noreply.github.com>
2026-09-07 12:03:25 +0200

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isAlpha(self, c):
+        return ('a' <= c <= 'z') or ('A' <= c <= 'Z') or ('0' <= c <= '9')
+
+    def isPalindrome(self, s: str) -> bool:
+        
+        i = 0
+        j = len(s) - 1
+
+        while i < j:
+
+            # check if left is NaN
+            while i < j and not self.isAlpha(s[i]):
+                i += 1
+
+            # check if right is NaN
+            while i < j and not self.isAlpha(s[j]):
+                j -= 1
+
+            # compare similarity
+            if s[i].lower() != s[j].lower():
+                return False
+
+            i += 1
+            j -= 1
+        
+        return True
